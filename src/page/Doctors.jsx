1d42cb3d1cb00db20754a2240888ff373b3a09fe
SOUMOJIT KOLEY <koleysoumojit6@gmail.com>
2024-04-12 01:23:39 +0530

    applied map on doctor's filter

--- src/page/Doctors.jsx
+++ src/page/Doctors.jsx
@@ -2,15 +2,67 @@ import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import data from '../data/doctorDetails.js'
 import Footer from '../components/Footer.jsx'
-import { FcApproval } from "react-icons/fc";
-import { useMediaQuery } from 'react-responsive';
+import { FcApproval } from "react-icons/fc"
+import { useMediaQuery } from 'react-responsive'
+import LocomotiveScroll from 'locomotive-scroll'
+const locomotiveScroll = new LocomotiveScroll()
 import './Doctors.css'
-import LocomotiveScroll from 'locomotive-scroll';
-const locomotiveScroll = new LocomotiveScroll();
 
 const Doctors = () => {
-  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isMobile = useMediaQuery({ maxWidth: 768 })
   const [doc, setDoc] = useState(data)
+
+  const filterType = [
+    {
+      name: 'All',
+      value: 'all',
+    },
+    {
+      name: 'General Physician',
+      value: 'General Physician',
+    },
+    {
+      name: 'General Surgeon',
+      value: 'General Surgeon',
+    },
+    {
+      name: 'General Medicine',
+      value: 'General Medicine',
+    },
+    {
+      name: 'Medicine Specialist',
+      value: 'Internal Medicine Specialist',
+    },
+    {
+      name: 'Pediatrician',
+      value: 'Pediatrician',
+    },
+    {
+      name: 'Child Specialist',
+      value: 'Child Specialist',
+    },
+    {
+      name: 'Neurologist',
+      value: 'Neurologist',
+    },
+    {
+      name: 'Gastroenterologist',
+      value: 'Gastroenterologist',
+    },
+    {
+      name: 'ENT Specialist',
+      value: 'ENT Specialist',
+    },
+    {
+      name: 'Urologist',
+      value: 'Urologist',
+    },
+    {
+      name: 'Gynaecologist',
+      value: 'Gynaecologist',
+    },
+  ]
+  
   function changeType(type) {
     if(isMobile) {
       locomotiveScroll.scrollTo(0, 0)
@@ -31,54 +83,14 @@ const Doctors = () => {
       <Navbar />
       <div className='doctor-div'>
         <div className='filter-part'>
-          <div className="each-filter" onClick={() => changeType('all')}>
-            <img src="/filterImage/0.png" alt="" />
-            <p>All</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('General Physician')}>
-            <img src="/filterImage/1.png" alt="" />
-            <p>General Physician</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('General Surgeon')}>
-            <img src="/filterImage/2.png" alt="" />
-            <p>General Surgeon</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('General Medicine')}>
-            <img src="/filterImage/3.png" alt="" />
-            <p>General Medicine</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('Internal Medicine Specialist')}>
-            <img src="/filterImage/4.png" alt="" />
-            <p>Medicine Specialist</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('Pediatrician')}>
-            <img src="/filterImage/5.png" alt="" />
-            <p>Pediatrician</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('Child Specialist')}>
-            <img src="/filterImage/6.png" alt="" />
-            <p>Child Specialist</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('Neurologist')}>
-            <img src="/filterImage/7.png" alt="" />
-            <p>Neurologist</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('Gastroenterologist')}>
-            <img src="/filterImage/8.png" alt="" />
-            <p>Gastroenterologist</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('ENT Specialist')}>
-            <img src="/filterImage/9.png" alt="" />
-            <p>ENT Specialist</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('Urologist')}>
-            <img src="/filterImage/10.png" alt="" />
-            <p>Urologist</p>
-          </div>
-          <div className="each-filter" onClick={() => changeType('Gynaecologist')}>
-            <img src="/filterImage/11.png" alt="" />
-            <p>Gynaecologist</p>
-          </div>
+          {
+            filterType.map((ef, idx) => (
+              <div key={idx} className="each-filter" onClick={() => changeType(`${ef.value}`)}>
+                <img src={`/filterImage/${idx}.png`} alt="" />
+                <p>{ef.name}</p>
+              </div>
+            ))
+          }
         </div>
         <div className='each-doctor-part' id='edp'>
           {
